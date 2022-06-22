1963ac6ce7726421119bbf3f4c142ee50e387029
Jerreme De Torres <jerremedetorres029@gmail.com>
2022-06-22 22:16:17 +0800

    create

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DoggosKitties.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
